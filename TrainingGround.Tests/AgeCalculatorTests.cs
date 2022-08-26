c1f53ff6c9613374d003f7e6c1b6e5122c3569b8
TanLui <effishing@gmail.com>
2022-08-26 15:32:35 +0200

    change GetAge to static method and update tests

--- TrainingGround.Tests/AgeCalculatorTests.cs
+++ TrainingGround.Tests/AgeCalculatorTests.cs
@@ -7,9 +7,13 @@ public class AgeCalculatorTests
     [Fact]
     public void someone_born_1972_is_50_in_2022()
     {
-        _calculator = new AgeCalculator();
-        // arrange
-        var age = _calculator.GetAge(1972, 2022);
+        // _calculator = new AgeCalculator();
+        // // arrange
+        // var age = _calculator.GetAge(1972, 2022);
+
+
+        // act
+        var age = AgeCalculator.GetAge(1972, 2022);
 
         // assert
         Assert.Equal(50, age);
@@ -18,10 +22,13 @@ public class AgeCalculatorTests
     [Fact]
     public void someone_born_2022_is_0_in_2022()
     {
-    // act
-    _calculator = new AgeCalculator();
-    var age = _calculator.GetAge(2022, 2022);
+    // // act
+    // _calculator = new AgeCalculator();
+    // var age = _calculator.GetAge(2022, 2022);
 
+
+    // act
+    var age = AgeCalculator.GetAge(2022, 2022);
     // assert
     Assert.Equal(0, age);
     }
--- TrainingGround/AgeCalculator.cs
+++ TrainingGround/AgeCalculator.cs
@@ -1,7 +1,17 @@
 ﻿namespace TrainingGround;
 public class AgeCalculator
 {
-  public int GetAge(int birthYear, int currentYear)
+  // --- comment out to test following w static method
+  // public int GetAge(int birthYear, int currentYear)
+  // {
+  //   return currentYear - birthYear;
+  // }
+
+// static method  belongs to the class, not to the instance., test will fail
+//error CS0176: Member 'AgeCalculator.GetAge(int, int)' cannot be accessed with an instance referen
+
+
+    public static int GetAge(int birthYear, int currentYear)
   {
     return currentYear - birthYear;
   }
