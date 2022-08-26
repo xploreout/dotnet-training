1eeb82e1a325cc132774bdcd7982e63cb24a9e59
TanLui <effishing@gmail.com>
2022-08-26 14:33:44 +0200

    add constructor

--- TrainingGround.Tests/PersonTests.cs
+++ TrainingGround.Tests/PersonTests.cs
@@ -2,9 +2,19 @@ public class PersonTests {
   [Fact]
   public void create_person_using_parameterless_constructor()
   {
-    var p = new Person("Marcus");
+    var p = new Person("Marcus", 2000);
 
     Assert.NotNull(p);
     Assert.Equal("Marcus", p.Name);
+  }
+
+  [Fact]
+  public void a_person_born_1972_is_50_2022()
+  {
+    var p = new Person("Tom", 1972);
+
+    var age = p.GetAge(2022);
+
+    Assert.Equal(50, age);
   }
 }
--- TrainingGround/Person.cs
+++ TrainingGround/Person.cs
@@ -1,13 +1,43 @@
 public class Person {
-  public string Name;
-  public int Age;
   public double LengthInMeters;
+  // public int Age;
   
-  public Person(string name)
+  //----------------
+  private string? _name;
+  public string? Name
+  {
+    get { return _name; }
+    set { this._name = value; }
+  }
+  //-------end  name------------
+
+  //---making birthYear public get and private set
+  private int? _birthYear;
+  public int? BirthYear 
+  {
+    get { return _birthYear;}
+    set { _birthYear = value; }
+  }
+  //---end birthYear---
+
+  //default..but as soon as we created Person w arg, we need to specific the fields
+  public Person() {
+    _name = "John";
+    _birthYear = 2000;
+  }
+  public Person(string name, int birthYear)
   {
     Name = name;
+    BirthYear = birthYear;
+    // Age = GetAge(2022)
   }
- }
+
+  public object GetAge(int currentYear)
+  {
+
+    return currentYear - BirthYear;
+  }
+}
 
 
 
